701591763f6dc61aac3a7bcc08f1ec5baf1a55b1
R3enox <129979133+R3enox@users.noreply.github.com>
2023-10-21 15:15:30 +0300

    do task 1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,45 @@
+import {
+  BlogCard,
+  Container,
+  Section,
+  Heading,
+  Statistics,
+  ForbesList,
+  CryptoHistory,
+} from 'components';
+import data from '../../data/article.json';
+
+export const App = () => {
+  return (
+    console.log(data) || (
+      <Section>
+        <Container>
+          <Heading marginBottom="50px" textAlign="center">
+            Task 1
+          </Heading>
+          <BlogCard
+            poster={data.poster}
+            tag={data.tag}
+            title={data.title}
+            description={data.description}
+            userName={data.name}
+            avatar={data.avatar}
+            postedAt={data.postedAt}
+          />
+          <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+            Task 2
+          </Heading>
+          <Statistics />
+          <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+            Task 3
+          </Heading>
+          <ForbesList />
+          <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+            Task 4
+          </Heading>
+          <CryptoHistory />
+        </Container>
+      </Section>
+    )
+  );
+};
